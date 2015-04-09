1062586c2872bc5fcda32a14e75fc6717334fdc4
EtienneCowley <etiennecowley@gmail.com>
2015-04-09 22:32:23 +0200

    Added framework classes

---
+++ EnterpirseMVVM.DesktopClient/ViewModels/CustomerViewModel.cs
@@ -0,0 +1,21 @@
+﻿using EnterpiseMVVM.Windows;
+using System.ComponentModel.DataAnnotations;
+
+namespace EnterpirseMVVM.DesktopClient.ViewModels
+{
+    public class CustomerViewModel : ViewModel
+    {
+        private string customerName;
+
+        [Required]
+        public string CustomerName
+        {
+            get { return customerName; }
+            set
+            {
+                customerName = value;
+                OnPropertyChanged();
+            }
+        }
+    }
+}
---
+++ EnterpiseMVVM.Windows/ObservableObject.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Runtime.CompilerServices;
+
+namespace EnterpiseMVVM.Windows
+{
+    public class ObservableObject : INotifyPropertyChanged
+    {        
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        public void OnPropertyChanged([CallerMemberName] string propertyName = "")
+        {
+            PropertyChangedEventHandler handler = PropertyChanged;
+            if (handler != null) handler(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
